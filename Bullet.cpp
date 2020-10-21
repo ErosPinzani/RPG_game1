9ed9644d8dfbbc904f90a3949f513e2b99234660
FrancescoFaillaceIng <71319628+FrancescoFaillaceIng@users.noreply.github.com>
2020-10-22 00:09:15 +0200

    Aggiunta classe bullet

---
+++ Bullet.cpp
@@ -0,0 +1,65 @@
+//
+// Created by Francesco on 22/10/2020.
+//
+
+#include "Bullet.h"
+
+const std::string &Bullet::getType() const {
+    return type;
+}
+
+void Bullet::setType(const std::string &type) {
+    Bullet::type = type;
+}
+
+float Bullet::getPosX() const {
+    return posX;
+}
+
+void Bullet::setPosX(float posX) {
+    Bullet::posX = posX;
+}
+
+float Bullet::getPosY() const {
+    return posY;
+}
+
+void Bullet::setPosY(float posY) {
+    Bullet::posY = posY;
+}
+
+float Bullet::getRange() const {
+    return range;
+}
+
+void Bullet::setRange(float range) {
+    Bullet::range = range;
+}
+
+int Bullet::getSpeed() const {
+    return speed;
+}
+
+void Bullet::setSpeed(int speed) {
+    Bullet::speed = speed;
+}
+
+int Bullet::getPower() const {
+    return power;
+}
+
+void Bullet::setPower(int power) {
+    Bullet::power = power;
+}
+
+bool Bullet::isHit() const {
+    return hit;
+}
+
+void Bullet::setHit(bool hit) {
+    Bullet::hit = hit;
+}
+
+void Bullet::move(float posX, float posY) {
+
+}
---
+++ Bullet.h
@@ -0,0 +1,52 @@
+//
+// Created by Francesco on 22/10/2020.
+//
+
+#ifndef RPG_GAME_BULLET_H
+#define RPG_GAME_BULLET_H
+#include <string>
+
+class Bullet {
+public:
+    void move(float posX,float posY);
+
+    const std::string &getType() const;
+
+    void setType(const std::string &type);
+
+    float getPosX() const;
+
+    void setPosX(float posX);
+
+    float getPosY() const;
+
+    void setPosY(float posY);
+
+    float getRange() const;
+
+    void setRange(float range);
+
+    int getSpeed() const;
+
+    void setSpeed(int speed);
+
+    int getPower() const;
+
+    void setPower(int power);
+
+    bool isHit() const;
+
+    void setHit(bool hit);
+
+private:
+    std::string type;
+    float posX;
+    float posY;
+    float range;
+    int speed;
+    int power;
+    bool hit;
+};
+
+
+#endif //RPG_GAME_BULLET_H
