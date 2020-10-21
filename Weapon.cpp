6170e7869c3f337ce5e8346a62d31144b6a1ead9
FrancescoFaillaceIng <71319628+FrancescoFaillaceIng@users.noreply.github.com>
2020-10-22 00:19:52 +0200

    Aggiunta classe weapon

---
+++ Weapon.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Francesco on 22/10/2020.
+//
+
+#include "Weapon.h"
+
+const std::string &Weapon::getType() const {
+    return type;
+}
+
+void Weapon::setType(const std::string &type) {
+    Weapon::type = type;
+}
---
+++ Weapon.h
@@ -0,0 +1,22 @@
+//
+// Created by Francesco on 22/10/2020.
+//
+
+#ifndef RPG_GAME_WEAPON_H
+#define RPG_GAME_WEAPON_H
+#include <string>
+
+class Weapon {
+public:
+    virtual void shoot() = 0;
+
+    const std::string &getType() const;
+
+    void setType(const std::string &type);
+
+private:
+    std::string type;
+};
+
+
+#endif //RPG_GAME_WEAPON_H
