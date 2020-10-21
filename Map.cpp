63f81bce0215eb909207e9c522a0ba1d67922b94
ErosPinzani <71319627+ErosPinzani@users.noreply.github.com>
2020-10-22 00:59:00 +0200

    Aggiunta classe Map

---
+++ Map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by erosp on 22/10/2020.
+//
+
+#include "Map.h"
---
+++ Map.h
@@ -0,0 +1,15 @@
+//
+// Created by erosp on 22/10/2020.
+//
+
+#ifndef RPG_GAME_MAP_H
+#define RPG_GAME_MAP_H
+
+
+class Map {
+public:
+    virtual void generatemap() = 0;
+};
+
+
+#endif //RPG_GAME_MAP_H
