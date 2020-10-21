763c8140b5eb9ffbcbce0194751ac597eea9a23a
ErosPinzani <71319627+ErosPinzani@users.noreply.github.com>
2020-10-22 00:49:25 +0200

    Aggiunta classe Enemy

---
+++ Enemy.cpp
@@ -0,0 +1,13 @@
+//
+// Created by erosp on 22/10/2020.
+//
+
+#include "Enemy.h"
+
+void Enemy::move(float posX, float posY) {
+
+}
+
+void Enemy::attack() {
+
+}
---
+++ Enemy.h
@@ -0,0 +1,16 @@
+//
+// Created by erosp on 22/10/2020.
+//
+
+#ifndef RPG_GAME_ENEMY_H
+#define RPG_GAME_ENEMY_H
+#include "Character.h"
+
+class Enemy: public Character {
+public:
+    void move(float posX, float posY) override;
+    void attack() override;
+};
+
+
+#endif //RPG_GAME_ENEMY_H
