8009c8cb189b1b0a92fad506a28c1104efeacf2e
ErosPinzani <71319627+ErosPinzani@users.noreply.github.com>
2020-10-22 00:00:04 +0200

    Aggiunta classe Character

---
+++ Character.cpp
@@ -0,0 +1,69 @@
+//
+// Created by erosp on 21/10/2020.
+//
+
+#include "Character.h"
+
+const std::string &Character::getType() const {
+    return type;
+}
+
+void Character::setType(const std::string &type) {
+    Character::type = type;
+}
+
+float Character::getPosX() const {
+    return posX;
+}
+
+void Character::setPosX(float posX) {
+    Character::posX = posX;
+}
+
+float Character::getPosY() const {
+    return posY;
+}
+
+void Character::setPosY(float posY) {
+    Character::posY = posY;
+}
+
+int Character::getHp() const {
+    return hp;
+}
+
+void Character::setHp(int hp) {
+    Character::hp = hp;
+}
+
+int Character::getDamage() const {
+    return damage;
+}
+
+void Character::setDamage(int damage) {
+    Character::damage = damage;
+}
+
+int Character::getStrength() const {
+    return strength;
+}
+
+void Character::setStrength(int strength) {
+    Character::strength = strength;
+}
+
+int Character::getArmor() const {
+    return armor;
+}
+
+void Character::setArmor(int armor) {
+    Character::armor = armor;
+}
+
+int Character::getSpeed() const {
+    return speed;
+}
+
+void Character::setSpeed(int speed) {
+    Character::speed = speed;
+}
---
+++ Character.h
@@ -0,0 +1,59 @@
+//
+// Created by Eros on 21/10/2020.
+//
+
+#ifndef RPG_GAME_CHARACTER_H
+#define RPG_GAME_CHARACTER_H
+#include <string>
+
+class Character {
+public:
+    virtual void move(float posX, float posY) = 0;
+
+    virtual void attack() = 0;
+
+    const std::string &getType() const;
+
+    void setType(const std::string &type);
+
+    float getPosX() const;
+
+    void setPosX(float posX);
+
+    float getPosY() const;
+
+    void setPosY(float posY);
+
+    int getHp() const;
+
+    void setHp(int hp);
+
+    int getDamage() const;
+
+    void setDamage(int damage);
+
+    int getStrength() const;
+
+    void setStrength(int strength);
+
+    int getArmor() const;
+
+    void setArmor(int armor);
+
+    int getSpeed() const;
+
+    void setSpeed(int speed);
+
+private:
+    std::string type;
+    float posX;
+    float posY;
+    int hp;
+    int damage;
+    int strength;
+    int armor;
+    int speed;
+};
+
+
+#endif //RPG_GAME_CHARACTER_H
