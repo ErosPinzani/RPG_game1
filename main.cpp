f6db6028187504eb7cc2652a3101e0680b05dd63
ErosPinzani <71319627+ErosPinzani@users.noreply.github.com>
2020-11-13 19:24:53 +0100

    SFML

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <SFMl/Graphics.hpp>
+#include <memory>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::VideoMode mode = sf::VideoMode::getDesktopMode();
+    std::shared_ptr<sf::RenderWindow> window(
+            new sf::RenderWindow(sf::VideoMode(1500, 850, mode.bitsPerPixel),
+                                 "RPG_game"));
+    window->setPosition(sf::Vector2i{(int) (mode.width - 1500) / 2,
+                                     (int) (mode.height - 850) / 2});                // Center window
+    window->setFramerateLimit(60);  // Set maximum framerate
+
+
+
+
+    while (window->isOpen()) {
+
+    }
     return 0;
 }
