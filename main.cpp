a089fdad31d02759e7e831dc81c1c60138ab009a
FrancescoFaillaceIng <71319628+FrancescoFaillaceIng@users.noreply.github.com>
2020-11-14 18:45:30 +0100

    creazione finestra SFML

--- main.cpp
+++ main.cpp
@@ -3,19 +3,23 @@
 #include <memory>
 
 int main() {
+    sf::Window window;
     sf::VideoMode mode = sf::VideoMode::getDesktopMode();
-    std::shared_ptr<sf::RenderWindow> window(
-            new sf::RenderWindow(sf::VideoMode(1500, 850, mode.bitsPerPixel),
-                                 "RPG_game"));
-    window->setPosition(sf::Vector2i{(int) (mode.width - 1500) / 2,
-                                     (int) (mode.height - 850) / 2});                // Center window
-    window->setFramerateLimit(60);  // Set maximum framerate
-
-
-
-
-    while (window->isOpen()) {
-
+    window.create(sf::VideoMode(1500, 850), "My window");
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
     }
+
+    window.setPosition(sf::Vector2i{(int) (mode.width - 1500) / 2,
+                                     (int) (mode.height - 850) / 2});                // Center window
+    window.setFramerateLimit(60);  // Set maximum framerate
+
+
     return 0;
 }
