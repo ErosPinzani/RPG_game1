84c147a0c14518a1f51cf13dfbbcc926c60e274b
ErosPinzani <71319627+ErosPinzani@users.noreply.github.com>
2020-12-09 14:02:56 +0100

    POV

--- Character.cpp
+++ Character.cpp
@@ -8,25 +8,25 @@
 void Character::UpdateMovement() {
     sprite.setPosition(rect.getPosition());
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && Character::Walkable(1)){
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) /*&& Character::Walkable(1)*/){
         rect.move(0, -movementSpeed);
         sprite.setTextureRect(sf::IntRect(counterWalking * 31.7, 31.7*3, 31.7, 31.7));
         direction = 1;
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)&& Character::Walkable(2)){
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) /*&& Character::Walkable(2)*/){
         rect.move(0, movementSpeed);
         sprite.setTextureRect(sf::IntRect(counterWalking * 31.7, 0, 31.7, 31.7));
         direction = 2;
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && Character::Walkable(3)){
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) /*&& Character::Walkable(3)*/){
         rect.move(-movementSpeed, 0);
         sprite.setTextureRect(sf::IntRect(counterWalking * 31.7, 31.7, 31.7, 31.7));
         direction = 3;
     }
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D )&& Character::Walkable(4)){
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) /*&& Character::Walkable(4)*/){
         rect.move(movementSpeed, 0);
         sprite.setTextureRect(sf::IntRect(counterWalking * 31.7, 31.7*2, 31.7, 31.7));
         direction = 4;
@@ -36,7 +36,7 @@ void Character::UpdateMovement() {
     if (counterWalking == 2)
         counterWalking = 0;
 }
-
+/*
 bool Character::Walkable (int direction) {
     int x =  rect.getPosition().x;
     int y = rect.getPosition().y;
@@ -59,3 +59,4 @@ bool Character::Walkable (int direction) {
         return false;
 
 }
+*/
