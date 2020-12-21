cc9940a721aed284048b50e4bbcfaae45ff1ed4c
ErosPinzani <71319627+ErosPinzani@users.noreply.github.com>
2020-12-21 13:11:36 +0100

    google test

--- Tests/mainTests.cpp
+++ Tests/mainTests.cpp
@@ -1,16 +1,11 @@
 //
 // Created by erosp on 07/12/2020.
 //
-/*
-#include "googletest/include/gtest/gtest.h"
 
-TEST(pippo, bau){
-
-}
+#include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest();
     return RUN_ALL_TESTS();
 }
 
-*/
